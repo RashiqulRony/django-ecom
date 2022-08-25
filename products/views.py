0a8c5d096af987186c4e93f373e511acd293262d
Rashiqul Islam <rashiqul@smeinformatics.com>
2022-08-25 18:11:36 +0600

    Product page paginate added

--- products/views.py
+++ products/views.py
@@ -1,12 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 
 def index(request):
     products = Product.objects.all()
+
+    p = Paginator(products, 12)
+    page_number = request.GET.get('page')
+
+    try:
+        page_obj = p.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = p.page(1)
+
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = p.page(p.num_pages)
+
+
     data = {
-        'products' : products
+        'products': page_obj
     }
     return render(request, 'products.html', data)
 
