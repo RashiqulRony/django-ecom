814ed68b095396270a865028a82b394d78416579
Rashiqul Islam <rashiqul@smeinformatics.com>
2022-08-29 18:22:28 +0600

    User auth

--- ronshop/settings.py
+++ ronshop/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
-    'users',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -74,18 +72,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ronshop.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djecom',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT': '3306',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -104,6 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "users.User"
+
+ACCOUNT_UNIQUE_EMAIL = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
@@ -117,7 +125,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- users/models.py
+++ users/models.py
@@ -1,11 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class User(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(max_length=255)
-    avatar = models.CharField(max_length=255, null=True)
-    is_active = models.BooleanField(False)
-    status = models.BooleanField(False)
+class User(AbstractUser):
+    is_vendor = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
+
+class Vendor(models.Model):
+    user = models.OneToOneField(User, related_name="vendor", on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+    avatar = models.ImageField(null=True)
+    address = models.CharField(null=True, max_length=400)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.username = None
+
+    @property
+    def __str__(self):
+        return self.username
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, related_name="customer", on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20)
+    avatar = models.ImageField(null=True)
+    address = models.ImageField(null=True, default=400)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.username = None
+
+    @property
+    def __str__(self):
+        return self.username
+
 
